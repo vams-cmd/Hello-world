3b95cbcd2cfbdde9439c1bfa0e6c21cd0f042bd3
sivakumarsagar <root@centos8.us-central1-a.c.aerobic-smithy-265817.internal>
2020-01-31 09:27:01 +0000

    success code

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM adoptopenjdk/openjdk8:alpine-slim
+MAINTAINER abc@gmail.com
+WORKDIR /opt/app
+COPY target/demoapp-0.0.1-SNAPSHOT.jar .
+EXPOSE 8080
+ENV JAVA_OPTIONS ''
+CMD java $JAVA_OPTIONS -jar demoapp-0.0.1-SNAPSHOT.jar
+
---
+++ src/main/java/com/example/sampleapp/demoapp/DemoappApplication.java
@@ -0,0 +1,40 @@
+package com.example.sampleapp.demoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+
+@RestController 
+@SpringBootApplication
+public class DemoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoappApplication.class, args);
+	}
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String getHello() {
+	return "Hello from Server" + "\n";
+	}
+
+	@RequestMapping(value = "/hello/{paramId}", method = RequestMethod.GET)
+	public String getHelloWithParam(@PathVariable String paramId) {
+	System.out.print("id is " + paramId);
+	return "Hello from Server with Param : " + paramId + "\n";
+	}
+	@RequestMapping(value = "/test/{paramId}", method = RequestMethod.GET)
+	public String getTestWithParam(@PathVariable String paramId) {
+	System.out.print("id is " + paramId);
+	return "Hello from Server with Param : " + paramId + "\n";
+	}
+	@RequestMapping(value = "/devops", method = RequestMethod.GET)
+        public String getDevops() {
+        return "Hello from Devops Server" + "\n";
+        }
+	@RequestMapping(value = "/devops/{paramId}", method = RequestMethod.GET)
+        public String getDevopsWithParam(@PathVariable String paramId) {
+        System.out.print("id is " + paramId);
+        return "Hello from Server with Param : " + paramId + "\n";
+        }
+
+}
